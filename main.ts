37c66d9a74f5aa98f8864b9d533b34f02728cba2
Desoppere <59663882+Desoppere@users.noreply.github.com>
2020-07-20 17:20:56 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,56 @@
+let dobbelsteen = 0
+input.onGesture(Gesture.Shake, function () {
+    dobbelsteen = randint(0, 5)
+    if (dobbelsteen == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (dobbelsteen == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (dobbelsteen == 2) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (dobbelsteen == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (dobbelsteen == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
